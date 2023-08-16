fea0c579966d2babea29ef81a48486a29941de69
mcantop <maciejcantop@icloud.com>
2023-08-16 14:06:37 +0200

    Initial Commit

---
+++ TTT2/TTT2App.swift
@@ -0,0 +1,17 @@
+//
+//  TTT2App.swift
+//  TTT2
+//
+//  Created by Maciej on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TTT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
